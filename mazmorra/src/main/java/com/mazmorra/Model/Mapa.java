da4e5d41b34331fc277f147af601121bc8f67e3d
MiguelGonSe <gonzalezsegurom@gmail.com>
2025-05-08 21:09:30 +0200

    Mapa crea enemigos tambien. Comentado en personaje el siguiente paso para seguir mañana.

--- mazmorra/src/main/java/com/mazmorra/Model/Mapa.java
+++ mazmorra/src/main/java/com/mazmorra/Model/Mapa.java
@@ -1,5 +1,7 @@
 package com.mazmorra.Model;
 
+import java.util.List;
+
 import com.mazmorra.Controllers.JuegoController;
 
 import javafx.scene.Node;
@@ -10,88 +12,109 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 
- /**
-  * Gestiona la construcción gráfica del tablero de juego a partir de una matriz de números enteros proveniente del DataReader.
-  * <p>
-  * Las imágenes correspondientes a "suelo" y "pared" son enrutadas de forma estática desde Resources. <br>
-  * La matriz se dibuja programáticamente mediante la creación de un elemento de tipo Grid, <br>
-  * que posteriormente conecta con la escena a través de un AnchorPane.
-  * </p>
-  * 
-  * @author Miguel González Seguro
-  * @author Lucía Fernández Florencio
-  * 
-  */
+/**
+ * Gestiona la construcción gráfica del tablero de juego a partir de una matriz
+ * de números enteros proveniente del DataReader.
+ * <p>
+ * Las imágenes correspondientes a "suelo" y "pared" son enrutadas de forma
+ * estática desde Resources. <br>
+ * La matriz se dibuja programáticamente mediante la creación de un elemento de
+ * tipo Grid, <br>
+ * que posteriormente conecta con la escena a través de un AnchorPane.
+ * </p>
+ * 
+ * @author Miguel González Seguro
+ * @author Lucía Fernández Florencio
+ * 
+ */
 
 public class Mapa {
-    private static String paredPath = "/com/mazmorra/Images/pared.png"; //La ruta relativa de img prescinde de src/main/resources, porque se recupera desde el classpath (desde target/classes)
+    private static String paredPath = "/com/mazmorra/Images/pared.png"; // La ruta relativa de img prescinde de
+                                                                        // src/main/resources, porque se recupera desde
+                                                                        // el classpath (desde target/classes)
     private static String sueloPath = "/com/mazmorra/Images/suelo.png";
-    private int[][] mapaMatriz; //Matriz de datos procedente de DataReader.java
+    private int[][] mapaMatriz; // Matriz de datos procedente de DataReader.java
 
-    /*Constructor parametrizado */
+    /* Constructor parametrizado */
     public Mapa(int[][] mapaMatriz) {
         this.mapaMatriz = mapaMatriz;
     }
 
     /**
      * Recupera la información de la matriz del mapa.
+     * 
      * @return la matriz del mapa.
      */
     public int[][] getMapaMatriz() {
         return mapaMatriz;
     }
-    
+
     /**
-     * Traslada la disposición de las celdas de tipo suelo y techo a formato gráfico en Scene Builder.
+     * Traslada la disposición de las celdas de tipo suelo y techo a formato gráfico
+     * en Scene Builder.
      * 
-     * @param gridPaneJuego estructura de tipo grid donde se dispondrán las imágenes. 
-     * @param stackPane nodo padre de gridPaneJuego, que determina su tamaño.
+     * @param gridPaneJuego estructura de tipo grid donde se dispondrán las
+     *                      imágenes.
+     * @param stackPane     nodo padre de gridPaneJuego, que determina su tamaño.
      * 
      */
-    public void generarTablero(GridPane gridPaneJuego, StackPane stackPaneJuego){
-        resetearGridPane(gridPaneJuego); //Limpia el tablero antes de cada regeneración.
-        if(resetearGridPane(gridPaneJuego) != null) {
+    public void generarTablero(GridPane gridPaneJuego, StackPane stackPaneJuego) {
+        resetearGridPane(gridPaneJuego); // Limpia el tablero antes de cada regeneración.
+        if (resetearGridPane(gridPaneJuego) != null) {
             addConstraints(gridPaneJuego);
         }
         double anchoStack = stackPaneJuego.getWidth();
 
-        for (int i = 0; i < mapaMatriz.length; i++) { 
+        for (int i = 0; i < mapaMatriz.length; i++) {
             for (int j = 0; j < mapaMatriz[i].length; j++) {
                 int celda = mapaMatriz[i][j];
-                ImageView imageView = new ImageView(); //Crea una vista gráfica por cada celda.
-    
+                ImageView imageView = new ImageView(); // Crea una vista gráfica por cada celda.
+
                 if (celda == 0) {
                     imageView.setImage(new Image(getClass().getResource(sueloPath).toExternalForm()));
-                    //Obtiene el recurso dentro de la ruta de clase, y convierte la referencia a una URL completa que JavaFX pueda interpretar. 
+                    // Obtiene el recurso dentro de la ruta de clase, y convierte la referencia a
+                    // una URL completa que JavaFX pueda interpretar.
                 } else {
                     imageView.setImage(new Image(getClass().getResource(paredPath).toExternalForm()));
                 }
-                gridPaneJuego.add(imageView, j, i); // GridPane: (NodeChild, int columna, int fila): j se tiene que especificar antes que i.
-                actualizarTamCelda(gridPaneJuego, mapaMatriz.length, anchoStack); 
+                gridPaneJuego.add(imageView, j, i); // GridPane: (NodeChild, int columna, int fila): j se tiene que
+                                                    // especificar antes que i.
+                actualizarTamCelda(gridPaneJuego, mapaMatriz.length, anchoStack);
             }
         }
     }
 
-
-    public void generarPersonajes(GridPane gridPanePersonajes, StackPane stackPaneJuego){
-        //Limpia el contenido anterior del gridPane (si lo hubiese).
+    public void generarPersonajes(GridPane gridPanePersonajes, StackPane stackPaneJuego) {
+        // Limpia el contenido anterior del gridPane (si lo hubiese).
         resetearGridPane(gridPanePersonajes);
         addConstraints(gridPanePersonajes);
-        
+
         double anchoStack = stackPaneJuego.getWidth();
-        ImageView entidadJugador = new ImageView(); 
-        String url = Proveedor.getInstance().getJugador().getRutaImagen();//Crea una vista gráfica por cada c
+        ImageView entidadJugador = new ImageView();
+        String url = Proveedor.getInstance().getJugador().getRutaImagen();// Crea una vista gráfica por cada c
         entidadJugador.setImage(new Image(getClass().getResource(url).toExternalForm()));
         System.out.println("Ruta obtenida: " + url);
-System.out.println("URL resuelta:");
+        System.out.println("URL resuelta:");
         gridPanePersonajes.add(entidadJugador, 0, 0);
         entidadJugador.setFitWidth(32);
         entidadJugador.setFitHeight(32);
-        //actualizarTamCelda(gridPanePersonajes, mapaMatriz.length, anchoStack); 
-        }
-        
-    
+        // actualizarTamCelda(gridPanePersonajes, mapaMatriz.length, anchoStack);
 
+        // Posiciones fijas para enemigos
+        int[][] posiciones = {{ 7, 7 }, { 7, 0 }, { 0, 7 } };
+
+        List<Enemigo> enemigos = Proveedor.getInstance().getListaEnemigos();
+
+        for (int i = 0; i < enemigos.size() && i < posiciones.length; i++) {
+            Enemigo enemigo = enemigos.get(i);
+            ImageView enemigoView = new ImageView();
+            String urlEnemigo = enemigo.getRutaImagen();
+            enemigoView.setImage(new Image(getClass().getResource(urlEnemigo).toExternalForm()));
+            enemigoView.setFitWidth(32);
+            enemigoView.setFitHeight(32);
+            gridPanePersonajes.add(enemigoView, posiciones[i][0], posiciones[i][1]);
+        }
+    }
 
     /**
      * Limpia el contenido y las restricciones del GridPane.
@@ -100,55 +123,55 @@ System.out.println("URL resuelta:");
      * @return el GridPane sin contenido.
      * 
      */
-    private GridPane resetearGridPane(GridPane gridPane){
+    private GridPane resetearGridPane(GridPane gridPane) {
         gridPane.getChildren().clear();
         gridPane.getColumnConstraints().clear();
         gridPane.getRowConstraints().clear();
 
         return gridPane;
     }
-    
+
     /**
-     * Añade las restricciones en las columnas y filas del GridPane sobre el que se trabaja.
+     * Añade las restricciones en las columnas y filas del GridPane sobre el que se
+     * trabaja.
      * 
      * @param gridPaneJuego el GridPane del tablero de juego.
      * 
      */
-    private void addConstraints(GridPane gridPane){
-        for (int i = 0; i < mapaMatriz.length; i++) { //Asume que el tablero es cuadrado.
-            ColumnConstraints cc = new ColumnConstraints(); 
+    private void addConstraints(GridPane gridPane) {
+        for (int i = 0; i < mapaMatriz.length; i++) { // Asume que el tablero es cuadrado.
+            ColumnConstraints cc = new ColumnConstraints();
             RowConstraints rc = new RowConstraints();
-            
-            cc.setPercentWidth(100.0 / mapaMatriz.length);//Establece que el grid tiene que ocupar todo el espacio disponible.
+
+            cc.setPercentWidth(100.0 / mapaMatriz.length);// Establece que el grid tiene que ocupar todo el espacio
+                                                          // disponible.
             rc.setPercentHeight(100.0 / mapaMatriz.length);
-            
-            gridPane.getColumnConstraints().add(cc);//Añade las restricciones al grid.
+
+            gridPane.getColumnConstraints().add(cc);// Añade las restricciones al grid.
             gridPane.getRowConstraints().add(rc);
         }
     }
-    
+
     /**
-     * Calcula el tamaño de cada celda del tablero en función de las dimensiones obtenidas de la matriz.
+     * Calcula el tamaño de cada celda del tablero en función de las dimensiones
+     * obtenidas de la matriz.
      * 
-     * @param gridPane estructura de tipo GridPane donde se dispondrán las imágenes de tipo suelo y pared. 
-     * @param size tamaño del mapa, suponiendo que es un cuadrado perfecto.
-     * @param anchoStack ancho disponible en la estructura AnchorPane para distribuir las celdas.
+     * @param gridPane   estructura de tipo GridPane donde se dispondrán las
+     *                   imágenes de tipo suelo y pared.
+     * @param size       tamaño del mapa, suponiendo que es un cuadrado perfecto.
+     * @param anchoStack ancho disponible en la estructura AnchorPane para
+     *                   distribuir las celdas.
      * 
      */
-    private void actualizarTamCelda(GridPane gridPane, int size, double anchoStack){
-         
-            double tamCelda = anchoStack / size; // size = mapaMatriz.length = columnas
-        
-            for (Node node : gridPane.getChildren()) {
-                if (node instanceof ImageView) {
-                    ((ImageView) node).setFitWidth(tamCelda);
-                    ((ImageView) node).setFitHeight(tamCelda);
-                }
+    private void actualizarTamCelda(GridPane gridPane, int size, double anchoStack) {
+
+        double tamCelda = anchoStack / size; // size = mapaMatriz.length = columnas
+
+        for (Node node : gridPane.getChildren()) {
+            if (node instanceof ImageView) {
+                ((ImageView) node).setFitWidth(tamCelda);
+                ((ImageView) node).setFitHeight(tamCelda);
             }
         }
     }
-
-
-
-
-
+}
--- mazmorra/src/main/java/com/mazmorra/Model/Personaje.java
+++ mazmorra/src/main/java/com/mazmorra/Model/Personaje.java
@@ -5,26 +5,36 @@ import com.mazmorra.TipoJugador;
 /**
  * Clase abstracta que representa un personaje genérico del juego.
  * 
- * Centraliza los atributos comunes de las clases Jugador y Enemigo, las cuales heredan de esta clase.
+ * Centraliza los atributos comunes de las clases Jugador y Enemigo, las cuales
+ * heredan de esta clase.
  * 
  * -La posición del personaje se establece y gestiona a través de ObjetoMapa.
- * -Los observers son implementados en las clases instanciables hijas: Jugador y Enemigo.
+ * -Los observers son implementados en las clases instanciables hijas: Jugador y
+ * Enemigo.
  * 
  */
 public abstract class Personaje {
-    /*Atributos comunes a 'Jugador' y 'Enemigo' : estadísticas de combate, nombre e imagen gráfica asociada. */
+    /*
+     * Atributos comunes a 'Jugador' y 'Enemigo' : estadísticas de combate, nombre e
+     * imagen gráfica asociada.
+     */
     protected String nombre;
-    protected int ataque; 
+    protected int ataque;
     protected int defensa;
     protected int vida;
     protected int velocidad;
     protected TipoJugador tipo;
-    protected String rutaImagen; 
-    /* En lugar de almacenar una Image asociada al Personaje, almacenamos la ruta del recurso
-     * y después generamos la imagen desde el controlador del juego, encapsulando funcionalidades
+    protected String rutaImagen;
+    // protected int columna;
+    // protected int fila;
+
+    /*
+     * En lugar de almacenar una Image asociada al Personaje, almacenamos la ruta
+     * del recurso
+     * y después generamos la imagen desde el controlador del juego, encapsulando
+     * funcionalidades
      * para depender menos de JavaFx.
      */
-    
 
     private static int vidaInicial = 5;
 
@@ -48,10 +58,11 @@ public abstract class Personaje {
         this.rutaImagen = rutaImagen;
     }
 
-    /*GETTERS Y SETTERS */
+    /* GETTERS Y SETTERS */
 
     /**
      * Obtiene el nombre del personaje.
+     * 
      * @return nombre del personaje.
      */
     public String getNombre() {
@@ -60,6 +71,7 @@ public abstract class Personaje {
 
     /**
      * Establece el nombre del personaje.
+     * 
      * @param nombre el nuevo nombre.
      */
     public void setNombre(String nombre) {
@@ -68,6 +80,7 @@ public abstract class Personaje {
 
     /**
      * Obtiene el ataque del personaje.
+     * 
      * @return el valor de ataque actual.
      */
     public int getAtaque() {
@@ -76,6 +89,7 @@ public abstract class Personaje {
 
     /**
      * Establece el valor de ataque del personaje.
+     * 
      * @param ataque el nuevo valor de ataque.
      */
     public void setAtaque(int ataque) {
@@ -84,6 +98,7 @@ public abstract class Personaje {
 
     /**
      * Obtiene la defensa del personaje.
+     * 
      * @return el valor de defensa actual.
      */
     public int getDefensa() {
@@ -92,6 +107,7 @@ public abstract class Personaje {
 
     /**
      * Establece el valor de defensa del personaje.
+     * 
      * @param nombre el nuevo valor de defensa.
      */
     public void setDefensa(int defensa) {
@@ -100,14 +116,16 @@ public abstract class Personaje {
 
     /**
      * Obtiene el nivel de vida del personaje.
+     * 
      * @return el número de vidas actual.
      */
     public int getVida() {
         return vida;
     }
-    
+
     /**
      * Establece el valor de vida del personaje.
+     * 
      * @param nombre el nuevo número de vidas.
      */
     public void setVida(int vida) {
@@ -115,21 +133,26 @@ public abstract class Personaje {
     }
 
     /**
-     * Obtiene la velocidad del personaje, un valor constante dependiente del tipo de personaje.
+     * Obtiene la velocidad del personaje, un valor constante dependiente del tipo
+     * de personaje.
+     * 
      * @return el valor de velocidad.
      */
     public int getVelocidad() {
         return velocidad;
     }
-    
-    /* setVelocidad no es necesario, puesto que ésta se establece en función del tipo de personaje
+
+    /*
+     * setVelocidad no es necesario, puesto que ésta se establece en función del
+     * tipo de personaje
      * a través del método calcularvelocidad().
      */
 
     /**
-     * Obtiene el tipo de jugador asociado al personaje: 
-     * - Arquero, Mago o Guerrero para el Jugador. 
-     * - Cíclope, Chthulu o Minotauro para el Enemigo. 
+     * Obtiene el tipo de jugador asociado al personaje:
+     * - Arquero, Mago o Guerrero para el Jugador.
+     * - Cíclope, Chthulu o Minotauro para el Enemigo.
+     * 
      * @return el tipo de jugador.
      */
     public TipoJugador getTipoJugador() {
@@ -138,8 +161,9 @@ public abstract class Personaje {
 
     /**
      * Establece el tipo de jugador asociado al personaje:
-     * - Arquero, Mago o Guerrero para el Jugador. 
-     * - Cíclope, Chthulu o Minotauro para el Enemigo.  
+     * - Arquero, Mago o Guerrero para el Jugador.
+     * - Cíclope, Chthulu o Minotauro para el Enemigo.
+     * 
      * @param tipo el tipo de jugador.
      */
     public void setTipoJugador(TipoJugador tipo) {
@@ -148,6 +172,7 @@ public abstract class Personaje {
 
     /**
      * Obtiene la ruta del archivo de imagen asociado al personaje.
+     * 
      * @return la ruta del archivo de imagen.
      */
     public String getRutaImagen() {
@@ -156,17 +181,36 @@ public abstract class Personaje {
 
     /**
      * Establece la ruta del archivo de imagen asociado al personaje.
+     * 
      * @param el nombre la ruta del archivo de imagen.
      */
-    public void setRutaImagen(String rutaImagen){
+    public void setRutaImagen(String rutaImagen) {
         this.rutaImagen = rutaImagen;
     }
-    
+
+    // public int getColumna() {
+    //     return columna;
+    // }
+
+    // public int getFila() {
+    //     return fila;
+    // }
+
+    // public void setColumna(int columna) {
+    //     this.columna = columna;
+    // }
+
+    // public void setFila(int fila) {
+    //     this.fila = fila;
+    // }
+
     /* RESTO DE MÉTODOS */
     /**
-     * Asigna al Jugador un valor de velocidad de tipo estático, dependiente del tipo de jugador seleccionado (arquero, guerrero o mago).
+     * Asigna al Jugador un valor de velocidad de tipo estático, dependiente del
+     * tipo de jugador seleccionado (arquero, guerrero o mago).
      * 
-     * @param tipo tipo de Jugador seleccionado, definido por el enum {@code TipoJugador}.
+     * @param tipo tipo de Jugador seleccionado, definido por el enum
+     *             {@code TipoJugador}.
      * @return el valor de la velocidad correspondiente a este tipo de jugador.
      */
     public static int calcularVelocidad(TipoJugador tipo) {
@@ -191,20 +235,20 @@ public abstract class Personaje {
         }
     }
 
-    
     /**
-     * Devuelve una cadena que contiene cada atributo del personaje junto con su valor.
+     * Devuelve una cadena que contiene cada atributo del personaje junto con su
+     * valor.
      */
     @Override
     public String toString() {
         return "Personaje { " +
-            "nombre='" + getNombre() + "'" +
-            ", ataque='" + getAtaque() + "'" +
-            ", defensa='" + getDefensa() + "'" +
-            ", vida='" + getVida() + "'" +
-            ", velocidad='" + getVelocidad() + "'" +
-            ", tipo='" + getTipoJugador() + "'" +
-            ", ruta imagen='" + getRutaImagen() + "'" +
-            "}";
+                "nombre='" + getNombre() + "'" +
+                ", ataque='" + getAtaque() + "'" +
+                ", defensa='" + getDefensa() + "'" +
+                ", vida='" + getVida() + "'" +
+                ", velocidad='" + getVelocidad() + "'" +
+                ", tipo='" + getTipoJugador() + "'" +
+                ", ruta imagen='" + getRutaImagen() + "'" +
+                "}";
     }
 }
